b85928e28d4dd9a4982a219b62a65f731fb61946
Flameberry <flameberrydev@gmail.com>
2024-03-21 22:20:48 +0530

    Completed QR code scanner.

--- lib/pages/scanner.dart
+++ lib/pages/scanner.dart
@@ -1,5 +1,8 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class QRScannerScreen extends StatefulWidget {
   const QRScannerScreen({super.key});
@@ -9,10 +12,46 @@ class QRScannerScreen extends StatefulWidget {
 }
 
 class _QRScannerScreenState extends State<QRScannerScreen> {
+  bool isURL(String text) {
+    final RegExp urlRegex = RegExp(
+        r'^(?:http|https):\/\/[\w\-]+(?:\.[\w\-]+)+[\w\-.,@?^=%&:/~\+#]*[\w\-@?^=%&/~\+#]$');
+    return urlRegex.hasMatch(text);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: MobileScanner(onDetect: (capture) {}),
+      child: MobileScanner(
+          controller: MobileScannerController(
+            detectionSpeed: DetectionSpeed.noDuplicates,
+            returnImage: true,
+          ),
+          onDetect: (capture) {
+            List<Barcode> barcodes = capture.barcodes;
+            final Uint8List? image = capture.image;
+            for (final barcode in barcodes) {
+              print('Barcode Found! ${barcode.rawValue}');
+            }
+
+            if (image != null) {
+              showDialog(
+                  context: context,
+                  builder: (context) {
+                    String barcodeText = barcodes.first.rawValue ?? "";
+                    bool isURL = this.isURL(barcodeText);
+
+                    return AlertDialog(
+                      // Check if the barcode is a URL and display it in a webview
+                      title: isURL
+                          ? InkWell(
+                              child: Text(barcodeText),
+                              onTap: () => launchUrlString(barcodeText))
+                          : Text(barcodeText),
+                      content: Image.memory(image),
+                    );
+                  });
+            }
+          }),
     );
   }
 }
